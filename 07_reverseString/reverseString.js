1edef2c1426430511c2583a90759fc5685884803
wayne <197704620+kaiser498@users.noreply.github.com>
2025-11-27 09:20:39 +0545

    Do exercise

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  stringArray = string.split("");
+  stringArraySort = stringArray.toReversed();
+  newStringArray = stringArraySort.join("");
+  return newStringArray;
 };
 
 // Do not edit below this line
