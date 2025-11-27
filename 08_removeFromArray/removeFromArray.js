66af9312de6daf2cdb3fd8772f3f89eb18e87678
wayne <197704620+kaiser498@users.noreply.github.com>
2025-11-27 16:58:32 +0545

    Do exercise

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, num1, num2) {
+  if (arguments.length > 3) {
+    arr = [];
+    return arr;
+  }
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] === num1 || arr[i] === num2) {
+      arr.splice(i, 1);
+      i--;
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
