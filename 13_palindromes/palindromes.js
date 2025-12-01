a7ddf94a6e418044f1c9eb4efa37542513f9a482
wayne <197704620+kaiser498@users.noreply.github.com>
2025-12-01 20:54:54 +0545

    Do exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let result = "";
+  for (i = 0; i < string.length; i++) {
+    let char = string[i];
+    if ((char >= "a" && char <= "z") || (char >= "A" && char <= "Z")) {
+      result += char;
+    }
+  }
+  let lowerCased = result.toLowerCase();
+  let stringArray = lowerCased.split("");
+  let reversed = stringArray.toReversed();
+  let joined = reversed.join("");
+  if (joined === lowerCased) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
