761be329a87f8dae766cf90a0c712743006e1a33
wayne <197704620+kaiser498@users.noreply.github.com>
2025-12-02 08:22:25 +0545

    Do exercise

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+function getAge(death, birth) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+}
+const findTheOldest = function (people) {
+  //   const sortOldest = people.sort((a, b) => {
+  //     let bLived = b.yearOfDeath - b.yearOfBirth;
+  //     let alived = a.yearOfDeath - a.yearOfBirth;
+  //     return bLived - alived;
+  //   });
+  //   return sortOldest[0];
+  return people.reduce((oldest, current) => {
+    const oldestAge = getAge(oldest.yearOfDeath, oldest.yearOfBirth);
+    const currentAge = getAge(current.yearOfDeath, current.yearOfBirth);
+    return oldestAge > currentAge ? oldest : current;
+  });
 };
 
 // Do not edit below this line
