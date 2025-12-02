1042859470338d5a680c091eab2b76cb66d12f75
wayne <197704620+kaiser498@users.noreply.github.com>
2025-12-02 07:52:57 +0545

    Do exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  let firstPrev = 1;
+  let secondPrev = 0;
+  if (num == 0) {
+    return 0;
+  }
+  if (num < 0) {
+    return "OOPS";
+  }
+  for (i = 2; i <= num; i++) {
+    let current = firstPrev + secondPrev;
+    secondPrev = firstPrev;
+    firstPrev = current;
+  }
+  return firstPrev;
 };
 
 // Do not edit below this line
